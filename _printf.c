420b126e0620b18cba67353ed2776fd21c5656cf
Vincent <vinkipko@gmail.com>
2022-11-24 00:16:39 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,74 +1,49 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
  *
- *   * @format: format.
+ *  * _printf - works like prinf
  *
- *    * Return: Printed chars.
+ *   * @format: the format string
+ *
+ *    * Return: number of bytes printed or -1 on error
  *
  *     */
 
 int _printf(const char *format, ...)
+{
+int i, total = 0;
+char c;
+va_list args;
+int (*print)(va_list *);
+va_start(args, format);
+if (!format)
+return (0);
+
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{
+i++;
+
+c = format[i];
+
+if (c == '%')
 
 {
+total += write(1, "%", 1);
 
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
-va_list list;
-char buffer[BUFF_SIZE];
-if (format == NULL)
-return (-1);
-
-va_start(list, format);
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_ind++] = format[i];
-if (buff_ind == BUFF_SIZE)
-print_buffer(buffer, &buff_ind);
-printed_chars++;
+continue;
 }
 
+print = get_f(c);
+
+total += print(&args);
+}
 else
-{
-print_buffer(buffer, &buff_ind);
-flags = get_flags(format, &i);
-width = get_width(format, &i, list);
-precision = get_precision(format, &i, list);
-size = get_size(format, &i);
-++i;
-printed = handle_print(format, &i, list, buffer,
-flags, width, precision, size);
-
-if (printed == -1)
-return (-1);
-printed_chars += printed;
+total += write(1, &format[i], 1);
 }
-}
-print_buffer(buffer, &buff_ind);
-va_end(list);
-return (printed_chars);
-}
-
-/**
- *
- *  * print_buffer - Prints the contents of the buffer if it exist
- *
- *   * @buffer: Array of chars
- *
- *    * @buff_ind: Index at which to add next char, represents the length.
- *
- *     */
-
-void print_buffer(char buffer[], int *buff_ind)
-
-{
-
-if (*buff_ind > 0)
-write(1, &buffer[0], *buff_ind);
-*buff_ind = 0;
+va_end(args);
+return(total);
 }
