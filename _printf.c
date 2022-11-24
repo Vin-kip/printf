073cff23eedfde04760682b7a625d50095659661
mbithuka-00 <mbithuka-00>
2022-11-24 09:23:42 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,49 +1,66 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- *
- *  * _printf - works like prinf
- *
- *   * @format: the format string
- *
- *    * Return: number of bytes printed or -1 on error
- *
- *     */
-
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
-int i, total = 0;
-char c;
-va_list args;
-int (*print)(va_list *);
-va_start(args, format);
-if (!format)
-return (0);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-i++;
+	if (format == NULL)
+		return (-1);
 
-c = format[i];
+	va_start(list, format);
 
-if (c == '%')
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
 
-{
-total += write(1, "%", 1);
+	print_buffer(buffer, &buff_ind);
 
-continue;
+	va_end(list);
+
+	return (printed_chars);
 }
 
-print = get_f(c);
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
 
-total += print(&args);
-}
-else
-total += write(1, &format[i], 1);
-}
-va_end(args);
-return(total);
+	*buff_ind = 0;
 }
